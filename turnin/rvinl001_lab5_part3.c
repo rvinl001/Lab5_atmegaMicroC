6cfd05bf9226386e2ab3632f3b9dd992c46166bc
Rizelle Vinluan <rvinl001@wch136-24.cs.ucr.edu>
2020-01-26 16:55:38 -0800

    will fix lab3 later

---
+++ turnin/rvinl001_lab5_part3.c
@@ -0,0 +1,171 @@
+/*	Author: rvinl001
+ *  Partner(s) Name: David Kecheung
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+
+enum States {init, Start, Inc, Dec, Hold1, Hold2, Hold3, Release, Both} States;
+unsigned char ctr = 0x00;
+void tick(){
+	switch (States)
+	{
+		case Start:
+			States = init;
+			break;
+		
+		case init:
+			if ((PINA & 0x03) == 0x01)
+			{
+				States = Inc;
+				break;
+			}else if ((PINA & 0x03) == 0x02){
+				States = Dec;
+				break;
+			}else if (PINA == 0x03){
+				States = Both;
+				break;
+			}
+			
+		case Inc:
+			if ((PINA & 0x03) == 0x01)
+			{
+				States = Hold1;
+				break;
+			}else if ((PINA & 0x03) == 0x03){
+				States = Both;
+				break;
+			}
+		//	else if (PINA == 0x00)
+		//	{
+		//		States = init;
+		//	}
+			
+		case Dec:
+			if ((PINA & 0x03) == 0x02)
+			{
+				States = Hold2;
+				break;
+			}else if ((PINA & 0x03) == 0x03){
+				States = Both;
+				break;
+			}
+		//	else if (PINA = 0x00)
+		//	{
+		//		States = init;
+		//	}
+		case Hold1:
+			if (PINA == 0x01){
+				States = Hold1;
+				break;
+			}
+			else if (PINA == 0x00){
+				States = Release;
+				break;
+			}
+			else if (PINA == 0x02) {
+				States = Both;
+				break;
+			}
+                case Hold2:
+                        if (PINA == 0x02){
+                                States = Hold2;
+                                break;
+                        }
+                        else if (PINA == 0x00){
+                                States = Release;
+                                break;
+                        }
+			else if (PINA == 0x01) {
+				States = Both;
+				break;
+			}
+
+		case Release:
+			if ((PINA & 0x03) == 0x01)
+			{
+				States = Inc;
+				break;
+			}else if (PINA == 0x02){
+				States = Dec;
+				break;
+			}
+			else if (PINA == 0x03){
+				States = Both;
+				break;
+			}
+		case Both:
+			if (PINA == 0x03) {
+				States = Hold3;
+				break;
+			}
+		//	else if (PINA == 0x00){
+		//		States = init;
+		//		break;
+		//	}
+		case Hold3:
+			if (PINA == 0x03) {
+				States = Hold3;
+				break;
+			}
+			else if (PINA == 0x00) {
+				States = Release;
+				break;
+			}
+		default:
+			break;
+			
+			
+	}
+	
+	switch(States) {   // State actions
+     	case init:
+        	ctr = 0x00;
+       	 	break;
+     	case Inc:
+		if (ctr < 9)
+			ctr++;
+        	break;
+     	case Dec:
+        	if (ctr > 0)
+			ctr--;
+        	break;
+	case Hold1:
+		break;
+	case Hold2:
+		break;
+	case Release:
+		break;
+	case Both:
+		ctr = 0;
+		break;
+	case Hold3:
+		ctr = 0;
+		break;
+     	default:
+        	break;
+   } // State actions
+	
+}
+
+
+int main(void)
+{
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
+    States = Start;
+    ctr = 0x00;	
+    while (1) 
+    {
+	tick();
+	PORTC = ctr;	
+    }
+}
